f736f6d5ade8328f8a0faffd2d25aae3e963d1ba
Amos wachira <amoswachira50@gmail.com>
2023-06-23 09:17:36 +0300

    Time: 62 ms (49.12%), Space: 17.1 MB (37.72%) - LeetHub

---
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxDepthAfterSplit(self, seq):
+        res = [None] * len(seq)
+        A = B = 0
+        for i, c in enumerate(seq):
+            if c == '(':
+                if A <= B:
+                    A += 1
+                    res[i] = 0
+                else:
+                    B += 1
+                    res[i] = 1
+            else:
+                if A >= B:
+                    A -= 1
+                    res[i] = 0
+                else:
+                    B -= 1
+                    res[i] = 1
+        return res
